c90006f68693f7beb8585c8bec244f902b77aa0d
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-23 18:34:55 +0800

    Final

--- exercise-14.py
+++ exercise-14.py
@@ -5,7 +5,7 @@ rows = 5
 
 # Nested loop
 for i in range(rows + 1, 0, -1):
-    for j in range(0, i + 1):
+    for j in range(0, i - 1):
     # Print asterisk
         print("*", end=" ")
     # Add new lines for each row
